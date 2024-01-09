f017d8ed58846491a53b65bdd2e085e0fecb5fdd
Gary Jarrel <gary@jarrel.com.au>
2024-01-09 11:49:34 +1100

    add script to cancel AWS batch jobs

---
+++ home/bin/private_executable_aws_cancel_jobs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the job queue name is provided as an argument
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <job-queue-name>"
+  exit 1
+fi
+
+job_queue_name="$1"
+
+for i in $(aws batch list-jobs --job-queue "$job_queue_name" --job-status runnable --output text --query "jobSummaryList[*].[jobId]")
+do
+  echo "Cancel Job: $i"
+  aws batch cancel-job --job-id $i --reason "Cancelling job."
+  echo "Job $i canceled"
+done
