70c60f3030630b9f669cc687b381d0fae6b060f9
Gary Jarrel <gary@jarrel.com.au>
2024-05-22 17:22:28 +1000

    add: mongo tools to the mongo tool installer script

--- home/bin/private_installers/executable_install_mongodb-tools.sh
+++ home/bin/private_installers/executable_install_mongodb-tools.sh
@@ -6,4 +6,5 @@ curl -fSsL https://pgp.mongodb.com/server-$VERSION.asc |
 echo "deb [signed-by=/usr/share/keyrings/atlascli.gpg arch=amd64,arm64] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/$VERSION multiverse" |
   sudo tee /etc/apt/sources.list.d/mongodb-org-$VERSION.list
 
-sudo apt-get update && sudo apt-get install -y mongodb-atlas
+sudo apt-get update && sudo apt-get install -y mongodb-atlas mongodb-org-tools
+
