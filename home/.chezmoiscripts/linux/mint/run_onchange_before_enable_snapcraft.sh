91da58494a8e7590d4f9a3132384ab7bb9a6a41a
Gary Jarrel <gary@jarrel.com.au>
2024-07-24 22:23:58 +1000

    add: script to enable snapcraft on Debian system

--- home/.chezmoiscripts/linux/mint/run_onchange_before_enable_snapcraft.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Enables Use of Snaps on Mint
-# https://snapcraft.io/docs/installing-snap-on-linux-mint
-
-sudo mv /etc/apt/preferences.d/nosnap.pref /tmp/
-sudo apt update
-sudo apt install snapd
